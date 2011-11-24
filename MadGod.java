5941821c4bd58e712219c0ae563f26187daefd61
David Mah <mahhaha@gmail.com>
2011-11-24 13:23:58 -0500

    Preparing MadGod, which will monitor the game

---
+++ MadGod.java
@@ -0,0 +1,19 @@
+import java.awt.Robot;
+import java.util.*;
+public class MadGod {
+
+  // Returns returning HP on a scale of 1 to 100
+  public int getHP() {
+
+  }
+
+  // Returns a List of enemy coordinates where current player is at (0, 0)
+  public List<Point> getEnemies() {
+
+  }
+
+  // Returns a List of ally coordinates where current player is at (0, 0)
+  public List<Point> getAllies() {
+
+  }
+}
