6ec6e166274e8fad132985561d2baf7a805fced0
David Mah <mahhaha@gmail.com>
2011-11-24 15:49:59 -0500

    Bar reading optimized
    - Changed to use BufferedImage and Raster
    - Added Level Progress Reading
    - Added MP Reading

--- MadGod.java
+++ MadGod.java
@@ -5,10 +5,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 public class MadGod {
 
-  private static int HP_Y = 625;
-  private static int HP_START_X   = 1015;
-  private static int HP_END_X     = 1185;
-  private static int HP_PRECISION = 6;
+  private static int RED   = 0;
+  private static int GREEN = 1;
+  private static int BLUE  = 2;
+
+  private static int LEVEL_Y = 600;
+  private static int HP_Y    = 625;
+  private static int MP_Y    = 650;
+  private static int BAR_START_X  = 1015;
+  private static int BAR_WIDTH    = 170;
+  private static int BAR_PRECISION = 1;
 
   private static int REST_TIME = 250;
 
@@ -21,33 +27,63 @@ public class MadGod {
 
   public MadGod() throws AWTException{
     control = new Robot();
+    control.mouseMove(BAR_START_X, MP_Y);
+    control.setAutoDelay(10);
   }
-  // Returns returning HP on a scale of 1 to 100
+  // Returns HP on a scale of 1 to 100
   public int getHP() {
+    return getBar(HP_Y, RED);
+  }
+
+  // Returns Level Progress on a scale of 1 to 100
+  public int getLevelProgress() {
+    return getBar(LEVEL_Y, GREEN);
+  }
+
+  public int getMP() {
+    return getBar(MP_Y, BLUE);
+  }
+
+  private int getBar(int height, int colorType) {
+    Rectangle bounds  = new Rectangle(BAR_START_X, height - 1, BAR_WIDTH, 2);
+    BufferedImage bar = control.createScreenCapture(bounds);
+    Raster pixels = bar.getData();
+
     int result = 0;
-    for(int x = HP_START_X; x < HP_END_X; x += HP_PRECISION) {
-      Color current = control.getPixelColor(x, HP_Y);
-      if(current.getRed() <= current.getBlue() || current.getRed() <= current.getGreen())
+    for(int x = 0; x < BAR_WIDTH; x += BAR_PRECISION) {
+      int[] pixel = new int[3];
+      pixels.getPixel(x, 1, pixel);
+      if(!barMaintained(pixel, colorType))
         break;
-      result += HP_PRECISION;
+      result += BAR_PRECISION;
     }
-    return (int)((1.0 * result / (HP_END_X - HP_START_X)) * 100);
+    return (int)((1.0 * result / BAR_WIDTH) * 100);
+  }
+
+  private boolean barMaintained(int[] pixel, int colorType) {
+    boolean correct = true;
+    for(int col = 0; col < 3; col++) {
+      if(colorType != col &&pixel[col] >= pixel[colorType])
+        correct = false;
+    }
+    return correct;
   }
 
   // Returns a List of enemy coordinates where current player is at (0, 0)
   public List<Point> getEnemies() {
-    return getMapType(0);
+    return getMapType(RED);
   }
 
   // Returns a List of ally coordinates where current player is at (0, 0)
   // Not well accurate because allies can clump
   public List<Point> getAllies() {
-    return getMapType(1);
+    return getMapType(GREEN);
   }
 
   private List<Point> getMapType(int colorType) {
     List<Point> results = new ArrayList<Point>();
-    BufferedImage map = control.createScreenCapture(new Rectangle(MAP_START_X, MAP_START_Y, MAP_LENGTH, MAP_LENGTH));
+    Rectangle bounds     = new Rectangle(MAP_START_X, MAP_START_Y, MAP_LENGTH, MAP_LENGTH);
+    BufferedImage map   = control.createScreenCapture(bounds);
     Raster pixels = map.getData();
 
     for(int x = 0; x < MAP_LENGTH; x += MAP_PRECISION) {
@@ -55,21 +91,25 @@ public class MadGod {
         int[] pixel = new int[3];
         pixels.getPixel(x, y, pixel);
 
-        boolean correct = true;
-        for(int col = 0; col < 3; col++) {
-          if(col == colorType) {
-            if(pixel[col] < 240)
-              correct = false;
-          } else {
-            if(pixel[col] > 15)
-              correct = false;
-          }
-        }
-        if(correct)
+        if(mapTargetIdentified(pixel, colorType))
           results.add(new Point(x, y));
       }
     }
     return results;
+  }
+
+  private boolean mapTargetIdentified(int[] pixel, int colorType) {
+    boolean correct = true;
+    for(int col = 0; col < 3; col++) {
+      if(col == colorType) {
+        if(pixel[col] < 240)
+          correct = false;
+      } else {
+        if(pixel[col] > 15)
+          correct = false;
+      }
+    }
+    return correct;
   }
 
   public void rest() {
